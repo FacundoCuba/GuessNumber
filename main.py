91994ba9187593d64e467a9f7617ced55e103d38
facundo <facundogcuba@gmail.com>
2022-02-08 18:29:42 -0300

    Se agregar la creacion de un numero aleatorio de n digitos.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import random
+
 def numeroParaAdivinar():
-    n = int(input("Ingrese el numero a adivinar: "))
-    return n
+    n = input("Ingrese la cantidad de digitos del numero a adivinar: ")
+    if n.isdigit():
+
+        digitos = 10**int(n)
+        numero_secreto = random.randint(digitos/10, digitos)
+        #print(numero_secreto)
+        return numero_secreto
+    else:
+        print("Intente ingresar un numero")
+        print("Pruebe de nuevo")
+        numeroParaAdivinar()
 
 def adivinando(numero):
     NumeroSecreto = str(numero)
@@ -10,40 +21,46 @@ def adivinando(numero):
         if i in l:
             NumeroOculto += "*"
     NumeroOculto = str(NumeroOculto)
-    #print(NumeroSecreto)
     print("")
     print("Adivine el siguiente numero:", NumeroOculto)
     print("")
-    busqueda(NumeroSecreto, NumeroOculto)
+    if NumeroSecreto != NumeroOculto:
+        busqueda(NumeroSecreto, NumeroOculto)
+    else:
+        print(NumeroOculto)
 
 def busqueda(NumeroSecreto, NumeroOculto):
-    n = str(input("Ingrese un numero para adivinar: "))
-    if n in NumeroSecreto:
-        print("entra al IF")
-        NumeroOculto = reemplazo(NumeroSecreto, NumeroOculto, n)
-        print(NumeroOculto)
-    else:
-        print(f"{n} No esta en el numero")
-        print("Intente de nuevo!")
-        print("")
-        busqueda(NumeroSecreto, NumeroOculto)
+    if NumeroSecreto != NumeroOculto:
+        n = str(input("Ingrese un numero para adivinar: "))
+        if n in NumeroSecreto:
+            reemplazo(NumeroSecreto, NumeroOculto, n)
+        elif n not in NumeroSecreto:
+            print(f"{n} No esta en el numero")
+            print("Intente de nuevo!")
+            print("")
+            busqueda(NumeroSecreto, NumeroOculto)
+    elif NumeroSecreto == NumeroOculto:
+        print(f"El numero es {NumeroOculto}")
 
 def reemplazo (str1, str2, n):
-    lista1 = list(str1)
     lista2 = list(str2)
-    for i in lista1:
-        try:
-            indice = str1.index(n)
-            print(lista2)
-            lista2.pop(indice)
-            lista2.insert(indice, n)
-            print(lista1)
-            print(lista2)
-        except ValueError:
-            print("Error")
-            break
-    #lista2[int(i)] = lista1[int(i)]
+    while True:
+        indice = str1.index(n)
+        lista2.pop(indice)
+        lista2.insert(indice, n)
+        str2 = ListaAString(lista2)
+        print(str2)
+        if str1 == str2:
+            print("Adivinado!")
+            quit()
+        elif str1 != str2:
+            busqueda(str1, str2)
 
+def ListaAString(l):
+    str1 = ""
+    for i in l:
+       str1 += i
+    return str1
 
 if __name__ == "__main__":
     adivinando(numeroParaAdivinar())
